c24e8ec229cb7be3263ae2cabc40ee84175967fc
Hacha_tq55@student.itstep.org <Hacha_tq55@student.itstep.org>
2024-10-07 20:51:14 +0300

    Добавьте файлы проекта.

---
+++ SP_1.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace SP_1._1
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            ConnectToServer();
+        }
+        static void ConnectToServer()
+        {
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 8888);
+                Console.WriteLine("Привет, сервер!");
+
+                NetworkStream stream = client.GetStream();
+
+                byte[] data = new byte[256];
+                StringBuilder responseData = new StringBuilder();
+                int bytes;
+
+
+                do
+                {
+                    bytes = stream.Read(data, 0, data.Length);
+                    responseData.Append(Encoding.ASCII.GetString(data, 0, bytes));
+                }
+                while (stream.DataAvailable);
+
+
+                Console.WriteLine("Текущее время с сервера: " + responseData.ToString());
+
+
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ TcpServer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace TcpServer
+{
+    class Program
+    {
+        static void Main()
+        {
+            StartServer();
+        }
+
+        public static void StartServer()
+        {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            int port = 8888;
+            TcpListener listener = new TcpListener(ipAddress, port);
+
+            try
+            {
+                listener.Start();
+                Console.WriteLine("Сервер запущен...");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    Console.WriteLine("Привет, клиент!");
+
+                    NetworkStream stream = client.GetStream();
+
+                    string currentTime = DateTime.Now.ToString();
+                    byte[] data = Encoding.ASCII.GetBytes(currentTime);
+
+                    stream.Write(data, 0, data.Length);
+                    Console.WriteLine("Отправлено время: " + currentTime);
+
+                    client.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            finally
+            {
+                listener.Stop();
+            }
+        }
+    }
+}
+
